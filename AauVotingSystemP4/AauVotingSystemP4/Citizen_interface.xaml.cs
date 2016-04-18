76bc28f90803b68ae4b19fe8871c2dccb9ecf39c
JensAarup <jaarup14@student.aau.dk>
2016-04-18 14:03:50 +0200

    Interface ændringer
    Jeg har lavet om på nogle af funtioner i mainpage, og tilføjet lidt til
    brugernes interface. Dette er for at teste om det virker med GitHub

---
+++ AauVotingSystemP4/AauVotingSystemP4/Citizen_interface.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AauVotingSystemP4
+{
+    /// <summary>
+    /// Interaction logic for Citizen_interface.xaml
+    /// </summary>
+    public partial class Citizen_interface : UserControl
+    {
+        public Citizen_interface()
+        {
+            InitializeComponent();
+        }
+
+        private void SeePreviouslyElections_Click(object sender, RoutedEventArgs e)
+        {
+            //Når man trykker på "See Previously Elections" skal man herinde definere hvad der skal ske.
+            //Dette her er en test på om det virker:
+            TestPreviouslyElection.Text = "Ja, det virker!";
+        }
+    }
+}
