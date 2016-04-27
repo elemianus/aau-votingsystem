4e56fa548265e165664bec2c986fabe4c1b2bd26
JensAarup <jaarup14@student.aau.dk>
2016-04-27 10:57:52 +0200

    Vote Class and ZipCode class
    Vote Class and ZipCode class has been added

---
+++ AauVotingSystemP4/AauVotingSystemP4/Vote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AauVotingSystemP4
+{
+    public class Vote
+    {
+        public int VotingOptionId { get; set; }
+        public string CandidateNominationDistrict { get; set; }
+
+        public Vote(int VotingOptionId, string CandidateNominationDistrict)
+        {
+            this.VotingOptionId = VotingOptionId;
+            this.CandidateNominationDistrict = CandidateNominationDistrict;
+        }
+    }
+}
---
+++ AauVotingSystemP4/AauVotingSystemP4/ZipCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AauVotingSystemP4
+{
+    class ZipCode
+    {
+        public int ZipCodeId { get; set; }
+        public ZipCode(int ZipcodeId)
+        {
+            this.ZipCodeId = ZipCodeId;
+        }
+    }
+}
