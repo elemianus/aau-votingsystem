b506b5a86ace8431f83e5f07284905eeca372136
DanielHS92 <dhsa14@student.aau.dk>
2016-04-27 11:44:58 +0200

    Same changes as in interface.

--- AauVotingSystemP4/AauVotingSystemP4/Citizen.cs
+++ AauVotingSystemP4/AauVotingSystemP4/Citizen.cs
@@ -8,13 +8,16 @@ namespace AauVotingSystemP4
 {
     public class Citizen
     {
-        public int Cpr { get; set; }
-        public bool Voteconducted { get; set; } 
 
-        public Citizen (int Cpr, bool Voteconducted)
+        public int Cpr { get { return cpr; } }
+        private int cpr;
+        public bool Voteconducted = false;
+
+        public Citizen(int cpr, bool Voteconducted)
         {
-            this.Cpr = Cpr;
+
             this.Voteconducted = Voteconducted;
+            this.cpr = cpr;
         }
     }
 }
--- AauVotingSystemP4/AauVotingSystemP4/CitizenModel.cs
+++ AauVotingSystemP4/AauVotingSystemP4/CitizenModel.cs
@@ -8,20 +8,39 @@ namespace AauVotingSystemP4
 {
     public class CitizenModel
     {
+
         private List<Citizen> seeUserVotes = new List<Citizen>();
         public bool HasUserVoted { get { return hasUserVoted; } }
         private bool hasUserVoted = false;
 
 
-        public CitizenModel(Citizen voted)
-        {
-            AddCitizen(voted);
-        }
-        public List<Citizen> SeeUserVotes()
+        public List<Citizen> CheckUserVotes()
         {
             return seeUserVotes;
         }
 
-        public bool CheckUserVotes(int Cpr, bool Voteconducted)
+        public bool CheckUserVotes(int CPR, bool VOTEconducted)
+        {
+            if (HasUserVoted)
+                return false;
+            for (int i = 0; i < seeUserVotes.Count(); i++)
+            {
+                if (seeUserVotes[i].Voteconducted == VOTEconducted && seeUserVotes[i].Cpr == CPR)
+
+                    return true; //The user has voted
+            }
+            return false; //The user has not voted
+        }
+
+
+
+        /// <summary>
+        /// Finalizes the citizen vote 
+        /// </summary>
+        public void CitizenVoted()
+        {
+            hasUserVoted = true;
+        }
     }
+
 }
