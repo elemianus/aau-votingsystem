c9fe6f7248af4ba35d255b56da0f7d57b58b2c89
elemianus <pmor14@student.aau.dk>
2016-04-27 11:11:38 +0200

    Initial setup of electionBoard

--- AauVotingSystemP4/AauVotingSystemP4/ElectionBoard.cs
+++ AauVotingSystemP4/AauVotingSystemP4/ElectionBoard.cs
@@ -6,20 +6,25 @@ using System.Threading.Tasks;
 
 namespace AauVotingSystemP4
 {
-    class ElectionBoard : NominationDistrict
+    /// <summary>
+    /// This class is surposed to handle the voting ballot for a specific nomination district.
+    /// </summary>
+    class ElectionBoard 
     {
-        public int EbID;
-        public string NomName;
-        public int[] NomZipcodes;
+        public int AssociatedNominationDistrict { get; }
+        public string NominationDistrictName { get { return nominationDistrictName; }}
+        private string nominationDistrictName;
 
-        public ElectionBoard (string NomName, int[] NomZipcodes, int EbID)
+
+        /// <summary>
+        /// When the electionboard is created it has no associated cip codes
+        /// </summary>
+        /// <param name="nominationDistrictName">The name of the discrict</param>
+        /// <param name="AssociatedNominationDistrict">The nomination district id surplied - this MUST be the same as in the database</param>
+        public ElectionBoard (string nominationDistrictName, int AssociatedNominationDistrict)
         {
-            this.NomName = NomName;
-            this.NomZipcodes = NomZipcodes;
-            this.EbID = EbID;
-
-           
-           
+            this.nominationDistrictName = nominationDistrictName;
+            this.AssociatedNominationDistrict = AssociatedNominationDistrict;
         }
     }
 }
