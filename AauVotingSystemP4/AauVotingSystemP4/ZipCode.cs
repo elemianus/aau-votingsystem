ffb72aa28d3d7484794c4c23eab78986500ba0e8
Gotmytowel <runequist@gmail.com>
2016-05-02 14:21:21 +0200

    Adding classes

--- AauVotingSystemP4/AauVotingSystemP4/ZipCode.cs
+++ AauVotingSystemP4/AauVotingSystemP4/ZipCode.cs
@@ -6,12 +6,44 @@ using System.Threading.Tasks;
 
 namespace AauVotingSystemP4
 {
-    class ZipCode
+    /// <summary>
+    /// Represents a zipcode and the people in that zip code
+    /// </summary>
+    public class ZipCode
     {
-        public int ZipCodeId { get; set; }
-        public ZipCode(int ZipcodeId)
+        public int ZipCodeId { get; }
+        private List<Citizen> citizensInThisZipCode = new List<Citizen>();
+        public ZipCode(int ZipCode)
         {
-            this.ZipCodeId = ZipCodeId;
+            ZipCodeId = ZipCode;
+        }
+
+        /// <summary>
+        /// Gets all citizens in this cipcode
+        /// </summary>
+        /// <returns>Returns all citizens in this zipcode</returns>
+        public List<Citizen> GetAllCitizens() {
+            return citizensInThisZipCode;
+        }
+
+        /// <summary>
+        /// Add list of Citizens to this zip code ONLY if the zip codes are the same
+        /// </summary>
+        /// <param name="input">The citizens to add</param>
+        public void AddListOfCitizensToZipCode(List<Citizen> input) {
+            foreach(Citizen item in input) { 
+                if(item.Zipcode == ZipCodeId)
+                    citizensInThisZipCode.AddRange(input);
+            }
+        }
+
+        /// <summary>
+        /// Add a citizen to zipcode ONLY IF the zip codes are the same 
+        /// </summary>
+        /// <param name="input">The citizen to add</param>
+        public void AddCitizenToZipCode(Citizen input) {
+            if(ZipCodeId == input.Zipcode)
+            citizensInThisZipCode.Add(input);
         }
     }
 }
