e0894406243b59177a6e8515229669597a4e94b7
DanielHS92 <dhsa14@student.aau.dk>
2016-05-10 15:43:56 +0200

    Added a mainwindow for election as well as a header for the design of it

---
+++ AauVotingSystemP4/AauVotingSystemP4/HeaderElectionAdmin.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AauVotingSystemP4
+{
+    /// <summary>
+    /// Interaction logic for HeaderElectionAdmin.xaml
+    /// </summary>
+    public partial class HeaderElectionAdmin : UserControl
+    {
+        public HeaderElectionAdmin()
+        {
+            InitializeComponent();
+
+        }
+        private void Homepage_Click(object sender, RoutedEventArgs e)
+        {
+            ElectionAdminMainWindow emw = new ElectionAdminMainWindow();
+            emw.Show();
+        }
+
+        private void Homepage_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
