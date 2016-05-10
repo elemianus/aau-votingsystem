4222cdd2cdf555959534f2f7ab5664477c6f1eb7
JensAarup <jaarup14@student.aau.dk>
2016-05-10 12:44:47 +0200

    Login

---
+++ AauVotingSystemP4/AauVotingSystemP4/LogIn.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AauVotingSystemP4
+{
+    /// <summary>
+    /// Interaction logic for LogIn.xaml
+    /// </summary>
+    public partial class LogIn : Window
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInCitizen_Click(object sender, RoutedEventArgs e)
+        {
+            LoginCitizen lic = new LoginCitizen();
+            Main.NavigationService.Navigate(lic);
+        }
+
+        private void LogInElectionboard_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ AauVotingSystemP4/AauVotingSystemP4/LoginCitizen.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AauVotingSystemP4
+{
+    /// <summary>
+    /// Interaction logic for LoginCitizen.xaml
+    /// </summary>
+    public partial class LoginCitizen : Page
+    {
+        
+        
+        public LoginCitizen()
+        {
+            InitializeComponent();
+        }
+
+        private void LogIn_Click(object sender, RoutedEventArgs e)
+        {
+            //var databaseConector = new DatabaseConnector();
+            //databaseConector.AddVotionOPtion(TypeInUsername, 2);
+            //try
+            //{
+            //    string Query "SQL" + this.TypeInUsername
+            //}
+            //catch (Exception wrongUsername)
+            //{
+
+            //}
+        }
+    }
+}
