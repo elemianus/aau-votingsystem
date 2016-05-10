87c249d729992c32e95f587ba6e3cf9d139c792e
Jarhoj <jarhoj10@student.aau.dk>
2016-05-10 15:20:12 +0200

    Lavet ny gridbox med election oversigt
    Fjernet "view" knap og gjort klar til listview, samt fundet kode til når
    data kan hentes ind fra databasen

--- AauVotingSystemP4/AauVotingSystemP4/SeePreviouslyElections.xaml.cs
+++ AauVotingSystemP4/AauVotingSystemP4/SeePreviouslyElections.xaml.cs
@@ -23,11 +23,21 @@ namespace AauVotingSystemP4
         public SeePreviouslyElections()
         {
             InitializeComponent();
-        }
+        //    Placeholder code for the databaseconnector bits
+        //    List<TodoItem> items = new List<TodoItem>();
+        //    items.Add(new TodoItem() { Title = "Total votes cast:" });
+        //    items.Add(new TodoItem() { Title = "" });
+        //    items.Add(new TodoItem() { Title = "" });
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+            //    electionResult.ItemsSource = items;
 
-        }
+            //}
+            //public class TodoItem
+            //{
+            //    public string Title { get; set; }
+            //    public string Title2 { get; set; }
+            }
+
+        
     }
 }
