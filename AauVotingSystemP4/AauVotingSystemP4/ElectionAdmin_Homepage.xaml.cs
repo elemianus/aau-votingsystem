7c6fc8197c1cba3a853f5897cbdb62d71c7f565e
DanielHS92 <dhsa14@student.aau.dk>
2016-05-10 14:03:11 +0200

    Changed ElectionAdmin_Homepage

---
+++ AauVotingSystemP4/AauVotingSystemP4/ElectionAdmin_Homepage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AauVotingSystemP4
+{
+    /// <summary>
+    /// Interaction logic for ElectionAdmin_Homepage.xaml
+    /// </summary>
+    public partial class ElectionAdmin_Homepage : Page
+    {
+        public ElectionAdmin_Homepage()
+        {
+            InitializeComponent();
+        }
+        private void ParliamentElection_Click(object sender, RoutedEventArgs e)
+        {
+            ElectionboardVotingBallot evb = new ElectionboardVotingBallot();
+            evb.Show();
+        }
+
+        private void createElection_Click(object sender, RoutedEventArgs e)
+        {
+            CreatingElection evb = new CreatingElection();
+            evb.Show();
+        }
+    }
+}
+
