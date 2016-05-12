d075b5ccd064b55f48bc6b3ead1fdebb874b1a50
Jarhoj <jarhoj10@student.aau.dk>
2016-05-12 13:44:27 +0200

    Party results er oppe

--- AauVotingSystemP4/AauVotingSystemP4/SeePreviouslyElections.xaml.cs
+++ AauVotingSystemP4/AauVotingSystemP4/SeePreviouslyElections.xaml.cs
@@ -28,18 +28,32 @@ namespace AauVotingSystemP4
             var databaseConnector = new DatabaseConnector();
             var listOfElections = databaseConnector.GetAllElections();
             var listOfListOfElections = new List<ListOfElections>();
-            foreach (var item in listOfElections) 
+            foreach (var item in listOfElections)
             {
                 listOfListOfElections.Add(new ListOfElections() { TypeOfElection = item.ElectionType, StartDate = item.StartDate.Year.ToString() });
             }
-      
 
-            List<Result> items = new List<Result>();
-            items.Add(new Result() { Name = "Venstre", Votes = 500000, Mandates = 12 });
-            items.Add(new Result() { Name = "Dansk Folkeparti", Votes = 300000, Mandates = 7 });
-            items.Add(new Result() { Name = "Alternativet", Votes = 200000, Mandates = 4 });
+            var partyResults = new ResultCalculator(2);
+            var resultListParty = partyResults.CalculateResultForParties();
+            var listOfPartyResults = new List<ListOfPartyResults>();
+            foreach (var item in resultListParty)
+            {
+                listOfPartyResults.Add(new ListOfPartyResults() { PartyVoteCount = item.AmountOfVotes });
+            }
+            //var candidateResults = new ResultCalculator(38);
+            //var resultListCandidate = candidateResults.CalculateResultsForCandidates();
+            //var listOfCandidateResults = new List<ListOfCandidateResults>();
+            //foreach (var item in resultListCandidate)
+            //{
+            //    listOfCandidateResults.Add(new ListOfCandidateResults() { CandidateVoteCount = item.AmountOfVotes });
+            //}
+
+            //List<Result> items = new List<Result>();
+            //items.Add(new Result() { Name = "Venstre", Votes = 500000, Mandates = 12 });
+            //items.Add(new Result() { Name = "Dansk Folkeparti", Votes = 300000, Mandates = 7 });
+            //items.Add(new Result() { Name = "Alternativet", Votes = 200000, Mandates = 4 });
             electionsListBox.ItemsSource = listOfListOfElections;
-            electionResult.ItemsSource = items;
+            electionResult.ItemsSource = listOfPartyResults;
 
 
         }
@@ -47,16 +61,19 @@ namespace AauVotingSystemP4
 
 
     }
-    public class Result
-    {
-        public string Name { get; set; }
-        public int Votes { get; set; }
-        public int Mandates { get; set; }
-    }
+
     public class ListOfElections
     {
         public string TypeOfElection { get; set; }
         public string StartDate { get; set; }
 
     }
+    public class ListOfPartyResults
+    {
+        public int PartyVoteCount { get; set; }
+    }
+    //public class ListOfCandidateResults
+    //{
+    //    public int CandidateVoteCount { get; set; }
 }
+
