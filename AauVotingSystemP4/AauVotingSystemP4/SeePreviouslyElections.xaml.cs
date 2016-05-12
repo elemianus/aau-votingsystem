4c860b660418724677e4256edbbc25c5c08869bf
Jarhoj <jarhoj10@student.aau.dk>
2016-05-12 12:27:56 +0200

    ListOfElections er forbundet med database

--- AauVotingSystemP4/AauVotingSystemP4/SeePreviouslyElections.xaml.cs
+++ AauVotingSystemP4/AauVotingSystemP4/SeePreviouslyElections.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,42 +19,44 @@ namespace AauVotingSystemP4
     /// <summary>
     /// Interaction logic for SeePreviouslyElections.xaml
     /// </summary>
-    public partial class PreviousElectionList : Page
-    {
-        ElectionListBox model = new ElectionListBox();
-
-    }
-    public class ElectionListBox
-    {
-
-
-    }
-
     public partial class SeePreviouslyElections : Page
     {
         public SeePreviouslyElections()
         {
             InitializeComponent();
-            //    Placeholder code for the databaseconnector bits
-            
-            
-            
+            var databaseConnector = new DatabaseConnector();
+            var listOfElections = databaseConnector.GetAllElections();
+            var listOfListOfElections = new List<ListOfElections>();
+            foreach (var item in listOfElections) 
+            {
+                listOfListOfElections.Add(new ListOfElections() { TypeOfElection = item.ElectionType, StartDate = item.StartDate.Year.ToString() });
+            }
+            var databaseResults = new DatabaseConnector();
+            var listOfResults = 
+
             //List<Result> items = new List<Result>();
-            //items.Add(new Result() { Name = "Venstre", Votes = 500000, Mandates = 12});
+            //items.Add(new Result() { Name = "Venstre", Votes = 500000, Mandates = 12 });
             //items.Add(new Result() { Name = "Dansk Folkeparti", Votes = 300000, Mandates = 7 });
             //items.Add(new Result() { Name = "Alternativet", Votes = 200000, Mandates = 4 });
-            //electionResult.ItemsSource = items;
-                
+            electionsListBox.ItemsSource = listOfListOfElections;
+            electionResult.ItemsSource = items;
+
 
         }
-        
 
-        
+
+
     }
     public class Result
     {
         public string Name { get; set; }
         public int Votes { get; set; }
         public int Mandates { get; set; }
+    }
+    public class ListOfElections
+    {
+        public string TypeOfElection { get; set; }
+        public string StartDate { get; set; }
+
     }
 }
