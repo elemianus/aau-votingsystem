3a8ef92d3cce8fec3d5b23ac93f142871465d0e6
elemianus <pmor14@student.aau.dk>
2016-05-12 16:01:02 +0200

    Added options to see nominationDistricts and zipcodes

--- AauVotingSystemP4/AauVotingSystemP4/ElectionAdmin_Homepage.xaml.cs
+++ AauVotingSystemP4/AauVotingSystemP4/ElectionAdmin_Homepage.xaml.cs
@@ -19,9 +19,23 @@ namespace AauVotingSystemP4
     /// </summary>
     public partial class ElectionAdmin_Homepage : Page
     {
+        private Election election;
+        private int currentNominationDistrictId;
         public ElectionAdmin_Homepage()
         {
             InitializeComponent();
+
+            var databaseConector = new DatabaseConnector();
+            election = databaseConector.GetElection(3);
+            var nominationDistrict = databaseConector.GetNominationDistrictsForElection(election);
+            var items = new List<NominationDistrictDisplayItem>();
+
+            foreach (var item in nominationDistrict)
+            {
+                items.Add(new NominationDistrictDisplayItem() { Name = item.Name, Mandates = item.NumberOfMandates, NominationDistrictId = item.NominationDistrictId });
+            }
+
+            nominationDistrictListView.ItemsSource = items;
         }
         //Here is the create election for sending you on 
         private void createElection_Click(object sender, RoutedEventArgs e)
@@ -34,6 +48,47 @@ namespace AauVotingSystemP4
         {
             AdministrateElection evb = new AdministrateElection();
             evb.Show();
+        }
+
+        public class NominationDistrictDisplayItem
+        {
+            public string Name { get; set; }
+            public int Mandates { get; set; }
+            public int NominationDistrictId { get; set; }
+        }
+
+        public class ZipCodeDisplayItem
+        {
+            public int ZipCode { get; set; }
+            public string Name { get; set; }
+        }
+
+
+        private void ListViewItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var item = sender as ListViewItem;
+            if (item != null && item.IsSelected)
+            {
+                var currentNominationDistrict = item.Content as NominationDistrictDisplayItem;
+                currentNominationDistrictId = currentNominationDistrict.NominationDistrictId;
+                ListZipCodes(currentNominationDistrictId);
+            }
+        }
+
+        private void NominationDistrictClick_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var item = sender as ListViewItem;
+            if (item != null && item.IsSelected)
+            {
+                
+            }
+        }
+
+        private void ListZipCodes(int nominationId)
+        {
+            var databaseConector = new DatabaseConnector();
+            List<ZipCode> zipCodes = databaseConector.GetAllZipCodesForNominationDistrict(nominationId, election.Election_ID);
+            ZipDistrictListView.ItemsSource = zipCodes;
         }
     }
 }
