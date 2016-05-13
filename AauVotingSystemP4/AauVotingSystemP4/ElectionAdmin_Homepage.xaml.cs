bbc98d98e3f44aa93f11cd5da6ab88c9d65f4581
elemianus <pmor14@student.aau.dk>
2016-05-13 18:32:30 +0200

    Merged Administrator layout files and deleted some

--- AauVotingSystemP4/AauVotingSystemP4/ElectionAdmin_Homepage.xaml.cs
+++ AauVotingSystemP4/AauVotingSystemP4/ElectionAdmin_Homepage.xaml.cs
@@ -23,31 +23,40 @@ namespace AauVotingSystemP4
     /// <summary>
     /// Interaction logic for ElectionAdmin_Homepage.xaml
     /// </summary>
-    public partial class ElectionAdmin_Homepage : Page ,ElectionAdminRefreshInterface
+    public partial class ElectionAdmin_Homepage : Page, ElectionAdminRefreshInterface
     {
-        private Election election;
+        private Election currentElection;
         private NominationDistrict currentNominationDistrict;
         private List<NominationDistrict> nominationDistricts;
         private ZipCode curentZipCode;
         public ElectionAdmin_Homepage()
         {
             InitializeComponent();
-            ListAllNominationDistricts();
+            ListAllElections();
+        }
 
+        public void ListAllElections()
+        {
+            var databaseConector = new DatabaseConnector();
+            var elections = databaseConector.GetAllElections();
+            electionListView.ItemsSource = elections;
         }
 
         public void ListAllNominationDistricts()
         {
-            var databaseConector = new DatabaseConnector();
-            election = databaseConector.GetElection(3);
-            nominationDistricts = databaseConector.GetNominationDistrictsForElection(election);
+            if (currentElection != null)
+            {
+                var databaseConector = new DatabaseConnector();
+                nominationDistricts = databaseConector.GetNominationDistrictsForElection(currentElection);
 
-            nominationDistrictListView.ItemsSource = nominationDistricts;
+                nominationDistrictListView.ItemsSource = nominationDistricts;
+            }
         }
 
-        private void ListAllCandidatesForNominationDistrict() {
+        private void ListAllCandidatesForNominationDistrict()
+        {
             var databaseConector = new DatabaseConnector();
-            CandidatesListview.ItemsSource = databaseConector.GetVotingOptionForNominationDistrict(currentNominationDistrict.NominationDistrictId, election.Election_ID);
+            CandidatesListview.ItemsSource = databaseConector.GetVotingOptionForNominationDistrict(currentNominationDistrict.NominationDistrictId, currentElection.Election_ID);
         }
 
         //Here is the create election for sending you on 
@@ -59,14 +68,17 @@ namespace AauVotingSystemP4
 
         private void admin_election_Click(object sender, RoutedEventArgs e)
         {
-            AdministrateElection evb = new AdministrateElection();
-            evb.Show();
+            if (currentElection != null)
+            {
+                AdministrateElectionEdit evb = new AdministrateElectionEdit(currentElection, this);
+                evb.Show();
+            }
         }
 
         public void ListZipCodes(int nominationId)
         {
             var databaseConector = new DatabaseConnector();
-            List<ZipCode> zipCodes = databaseConector.GetAllZipCodesForNominationDistrict(nominationId, election.Election_ID);
+            List<ZipCode> zipCodes = databaseConector.GetAllZipCodesForNominationDistrict(nominationId, currentElection.Election_ID);
             ZipDistrictListView.ItemsSource = zipCodes;
         }
 
@@ -84,7 +96,7 @@ namespace AauVotingSystemP4
 
         private void createNominationDistrictButton_Click(object sender, RoutedEventArgs e)
         {
-            AddNominationDistrict addNominationDistrict = new AddNominationDistrict(election,this);
+            AddNominationDistrict addNominationDistrict = new AddNominationDistrict(currentElection, this);
             addNominationDistrict.Show();
         }
 
@@ -92,7 +104,7 @@ namespace AauVotingSystemP4
         {
             if (currentNominationDistrict != null) //Check if nomination district is selected
             {
-                AddNominationDistrict addNominationDistrict = new AddNominationDistrict(election,this, currentNominationDistrict.NominationDistrictId);
+                AddNominationDistrict addNominationDistrict = new AddNominationDistrict(currentElection, this, currentNominationDistrict.NominationDistrictId);
                 addNominationDistrict.Show();
             }
         }
@@ -101,30 +113,38 @@ namespace AauVotingSystemP4
         {
             if (currentNominationDistrict != null) //Check if nomination district is selected
             {
-                var dbConector = new DatabaseConnector();
 
-                if (currentNominationDistrict != null)
+                var dbConector = new DatabaseConnector();
+                //We must check if there is any remaning voteoptions in the nomination district. If there is the election administrator does not have the rights to delete it
+                if (dbConector.GetVotingOptionForNominationDistrict(currentNominationDistrict.NominationDistrictId, currentElection.Election_ID).Count == 0)
                 {
-                    dbConector.DeleteNominationDistrict(currentNominationDistrict);
-                    ListAllNominationDistricts();
+                    if (currentNominationDistrict != null)
+                    {
+                        dbConector.DeleteNominationDistrict(currentNominationDistrict);
+                        ListAllNominationDistricts();
+                    }
+                    currentNominationDistrict = null;
                 }
-                currentNominationDistrict = null;
+                else {
+                    MessageBox.Show("You cannont delete the nomination district, before it is out of voting options");
+                }
             }
-            
         }
 
         private void createZipCodeButton_Click(object sender, RoutedEventArgs e)
         {
-            if (currentNominationDistrict != null) { 
-            var window = new AddZipCodeElectionAdmin(election, currentNominationDistrict, this);
-            window.Show();
+            if (currentNominationDistrict != null)
+            {
+                var window = new AddZipCodeElectionAdmin(currentElection, currentNominationDistrict, this);
+                window.Show();
             }
         }
 
         private void editZipCodeButton_Click(object sender, RoutedEventArgs e)
         {
-            if (currentNominationDistrict != null && curentZipCode != null) {
-                var window = new AddZipCodeElectionAdmin(election, currentNominationDistrict, this, curentZipCode);
+            if (currentNominationDistrict != null && curentZipCode != null)
+            {
+                var window = new AddZipCodeElectionAdmin(currentElection, currentNominationDistrict, this, curentZipCode);
                 window.Show();
             }
         }
@@ -138,10 +158,22 @@ namespace AauVotingSystemP4
 
         private void removeButton_Click(object sender, RoutedEventArgs e)
         {
-            if (curentZipCode != null) {
+            if (curentZipCode != null && currentNominationDistrict != null)
+            {
                 var dbConector = new DatabaseConnector();
                 dbConector.DeleteZipCode(curentZipCode, currentNominationDistrict);
                 ListZipCodes(currentNominationDistrict.NominationDistrictId);
+            }
+        }
+
+        private void electionListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listBox = sender as ListBox;
+            var selectedItem = listBox.SelectedItem as Election;
+            currentElection = selectedItem;
+            if (currentElection != null)
+            {
+                ListAllNominationDistricts();
             }
         }
     }
